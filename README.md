5afd04cf6b3338bffacc6f143d8f5b363d878eb5
salvacam <salvacam@gmail.com>
2019-09-14 00:12:31 +0200

    first version

---
+++ README.md
@@ -0,0 +1,6 @@
+<h3>RSS Reader</h3>
+<hr/>
+
+Lector de RSS.
+
+<a href="https://salvacam.github.io/rssReader" target="_blank">Web</a>
---
+++ js/main.js
@@ -0,0 +1,165 @@
+document.addEventListener('DOMContentLoaded', function () {
+  	app.init();
+});
+
+var app = {  
+	
+	URL_SERVER: "https://calcicolous-moonlig.000webhostapp.com/rssReader/index.php?time=",
+	//URL_SERVER: "http://localhost:1234/back/index.php?time=",
+	
+  	clearRSS: document.getElementById('clearRSS'),
+  	updateRSS: document.getElementById('updateRSS'),
+	modalClear: document.getElementById('modalClear'),  
+	principalDiv: document.getElementById('principalDiv'),
+	  
+	consultationTime: 0,
+	
+	replaceText: function(content, key, value) {
+		return content.replace("{{"+ key +"}}", value ? value : "");
+	},
+
+	replaceTemplate: function(template, object) {
+		for(var key in object) {
+		  template = app.replaceText(template, key, object[key]);
+		}
+		return template;
+	},
+	
+	convertDate: function(unixtimestamp){	   
+		// Convert timestamp to milliseconds
+		let date = new Date(unixtimestamp*1000);
+	   
+		// Year
+		let year = date.getFullYear();
+	   
+		// Month
+		let month = date.getMonth() +1;
+	   
+		// Day
+		let day = date.getDate();
+	   
+		// Hours
+		let hours = date.getHours();
+	   
+		// Minutes
+		let minutes = "0" + date.getMinutes();
+	   
+		// Seconds
+		let seconds = "0" + date.getSeconds();
+	   
+		// Display date time in MM-dd-yyyy h:m:s format
+		let convdataTime = day+'-'+month+'-'+year+' '+hours + ':' + minutes.substr(-2);
+		
+		return convdataTime;		
+	},
+
+	clearDiv: function() {
+		principalDiv.innerHTML = '';
+	},
+	
+	showData: function() {
+		let dataRSS = "";		
+
+		if(localStorage.getItem('_rssReader_Data')) {
+
+			app.clearDiv();
+
+			dataRSS = JSON.parse(localStorage.getItem('_rssReader_Data'));
+
+			dataRSS.forEach(function(feedRSS) {
+				
+				let nameFeedTemplate = document.getElementById("nameFeedTemplate").innerHTML;
+				nameFeedTemplate = app.replaceTemplate(nameFeedTemplate, {
+					"nameFeed": feedRSS.name
+				  });
+				  
+				let element = document.createElement('div');
+				element.innerHTML = nameFeedTemplate;
+
+				feedRSS.content.forEach(function(itemRSS) {	
+					
+					let itemFeedTemplate = document.getElementById("itemFeedTemplate").innerHTML;
+					itemFeedTemplate = app.replaceTemplate(itemFeedTemplate, {
+						"urlItem": itemRSS.url,
+						"nameItem": itemRSS.title.replace("<![CDATA[", "").replace("]]>", ""),
+						"dateItem": app.convertDate(itemRSS.date)
+				  	});
+
+					let elementItem = document.createElement('div');
+					elementItem.classList.add('flex');
+					elementItem.innerHTML = itemFeedTemplate;
+					element.appendChild(elementItem);
+				});
+
+				app.principalDiv.appendChild(element);
+			});
+
+		}
+	},
+
+	getData: function() {
+			
+		app.updateRSS.classList.add('hide');
+
+		try {
+		  fetch(app.URL_SERVER+app.consultationTime)
+		  .then(
+			function(response) {
+	
+				app.updateRSS.classList.remove('hide');			  	
+	
+				response.text()
+			  	.then(function(text) {
+					localStorage.setItem('_rssReader_Data', text);
+			  		app.showData();
+			  	})
+			}
+		  );
+		} catch (err) {
+		  //app.showLastData();
+		}
+	},
+
+  	init: function() {  		
+
+  		if (localStorage.getItem("_rssReader_Time") && localStorage.getItem("_rssReader_Time")!=''){
+			app.consultationTime = localStorage.getItem("_rssReader_Time");
+			localStorage.setItem('_rssReader_Time', app.consultationTime);
+		} else {
+			localStorage.setItem('_rssReader_Time', app.consultationTime);
+		}
+
+		app.showData();
+		  
+		app.clearRSS.addEventListener('click', (event) => {
+	  		app.modalClear.classList.remove('hide');
+
+  			document.getElementById('okClear').addEventListener('click', () => {
+				app.modalClear.classList.add('hide');
+				localStorage.setItem('_rssReader_Data', '');
+				app.clearDiv();
+				document.getElementById('okClear').removeEventListener('click', ()=> {});
+  			});
+
+  			document.getElementById('closeClear').addEventListener('click', () => {  				
+				app.modalClear.classList.add('hide');
+				document.getElementById('closeClear').removeEventListener('click', ()=> {});
+  			});		    
+		});
+
+	  	app.updateRSS.addEventListener('click', (event) => {			
+			app.getData();
+			
+			app.consultationTime = Math.floor(Date.now() / 1000);			
+			localStorage.setItem('_rssReader_Time', app.consultationTime);
+		});
+
+		if ('serviceWorker' in navigator) {
+      		navigator.serviceWorker
+        		.register('service-worker.js')
+        		.then(function() {
+          		//console.log('Service Worker Registered');
+        	});
+		}
+  	}
+};
